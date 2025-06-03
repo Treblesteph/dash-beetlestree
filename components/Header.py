bbe2e26b8f45c746f219b28507b3289a8babe706
Steph <steph.unna@gmail.com>
2025-06-03 20:57:32 +0100

    add tooltip for hamburger

--- components/Header.py
+++ components/Header.py
@@ -4,7 +4,14 @@ def create_header(logo):
     header = dmc.AppShellHeader(
         dmc.Group(
             [
-                dmc.Burger(id="burger", size="sm", hiddenFrom="sm", opened=False),
+                dmc.Tooltip(
+                    label="Click to see images!",
+                    position="right",
+                    color="orange.6",
+                    withArrow=True,
+                    opened=True,
+                    children=dmc.Burger(id="burger", size="sm", hiddenFrom="sm", opened=False),
+                ),
                 dmc.Image(src=logo, style={"height": "40px", "width": "auto"}),
                 dmc.Title(
                     "Beetles Tree of Life",
